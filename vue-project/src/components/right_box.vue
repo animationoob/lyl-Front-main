3e587ec6a935d59065f4e75e0f03f5929f01d982
backt666 <2459482384@qq.com>
2024-11-09 13:04:47 +0800

    11.9

--- vue-project/src/components/right_box.vue
+++ vue-project/src/components/right_box.vue
@@ -3,6 +3,7 @@ import { ref, onMounted, onUnmounted } from 'vue'
 import Table from '@/components/Lists/table.vue'
 import group2 from '@/components/groups/group2.vue';
 import weatherdata from '@/assets/weatherData.json'
+import axios from 'axios';
 
 const data = ref({
     "head": [],
@@ -11,28 +12,37 @@ const data = ref({
 })
 
 // 用于存储从服务器获取的数据
-const newData = ref([]);
-
-// 定时获取数据的函数
-const fetchData = async () => {
-    try {
-        const response = await fetch('http://your-server-url/api/data'); // 替换为你的API地址
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const jsonData = await response.json();
-        newData.value = jsonData;
-    } catch (error) {
-        console.error('There was a problem with your fetch operation:', error);
-    }
-};
+const newData = ref()
 
 // 设置定时器，每隔10秒获取一次数据
-const intervalId = setInterval(fetchData, 10000);
+const getData = () =>{
+    axios.get('http://47.102.108.198:8899/get')
+        .then(re => {
+            newData.value = {
+                "header": ["日期", "属性", "值"],
+                "data": [
+                    ["温度", re.data.Temperature, "正常"],
+                    ["湿度", re.data.Humidity, "正常"],
+                    ["风速", re.data.WindSpeed, "正常"],
+                    ["风向", re.data.Direction, "正常"],
+                    ["气压", re.data.AirPressure, "正常"],
+                    ["光照", re.data.Lighting, "正常"],
+                    ["降雨量", re.data.Rainfall, "正常"]
+                ],
+                "index": true,
+                "columnWidth": [50],
+                "align": ["center"]
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+let intervalId = null;
 
 // 在组件挂载时设置定时器
 onMounted(() => {
-    fetchData(); // 初始加载也获取一次数据
+    getData();
+    intervalId = setInterval(getData, 5000);
 });
 
 // 在组件销毁时清除定时器
@@ -66,10 +76,10 @@ onUnmounted(() => {
                 <group2></group2>
             </div>
             <div class="center-list">
-                <dv-scroll-board :config="config" style="width:90%;height:90%" />
+                <dv-scroll-board :config="newData" style="width:90%;height:90%" />
             </div>
         </dv-border-box8>
-        <div style="width: 90%;height: 3px;background-color: #ccc;position: absolute;top: 35.5%;left: 5%;"/>
+        <div style="width: 90%;height: 3px;background-color: #ccc;position: absolute;top: 35.5%;left: 5%;" />
     </div>
 </template>
 
@@ -95,7 +105,7 @@ onUnmounted(() => {
         align-items: center;
     }
 
-    .time-list{
+    .time-list {
         width: 100%;
         height: 25%;
         background-color: rgb(0, 0, 0, 0);
@@ -104,7 +114,7 @@ onUnmounted(() => {
         align-items: center;
     }
 
-    .center-list{
+    .center-list {
         width: 100%;
         height: 65%;
         background-color: rgb(0, 0, 0, 0);
